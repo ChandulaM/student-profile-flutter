fc339639eaea16ea8246ee7feec087625117cce9
Dilhan Dharmasinghe <dilhannadun777@gmail.com>
2022-03-25 14:44:48 +0530

    forms implementaion started

--- lib/screens/admin/manage_teachers.dart
+++ lib/screens/admin/manage_teachers.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:student_profile/common/header.dart';
 
 class ManageTeacher extends StatefulWidget {
   const ManageTeacher({Key? key}) : super(key: key);
@@ -8,10 +11,263 @@ class ManageTeacher extends StatefulWidget {
 }
 
 class _ManageTeacherState extends State<ManageTeacher> {
+  String dropdownValue = '10A';
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Manage Teachers'),
+    final ButtonStyle style1 =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    final _formKey = GlobalKey<FormState>();
+
+    var name = "";
+    var email = "";
+    var password = "";
+
+    final nameController = TextEditingController();
+    final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+
+    @override
+    void dispose() {
+      nameController.dispose();
+      emailController.dispose();
+      passwordController.dispose();
+      super.dispose();
+    }
+
+    clearText() {
+      nameController.clear();
+      emailController.clear();
+      passwordController.clear();
+    }
+
+    CollectionReference students =
+        FirebaseFirestore.instance.collection('students');
+
+    Future<void> addUser() {
+      return students
+          .add({'name': name, 'email': email, 'password': password})
+          .then((value) => print('User Added'))
+          .catchError((error) => print('Failed to Add user: $error'));
+    }
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 10.0, vertical: 8.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Header(
+                          title: "Manage Teachers",
+                        ),
+                        SizedBox(
+                          height: 12.0,
+                        ),
+                      ]),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: ElevatedButton(
+                    style: style1,
+                    onPressed: () {},
+                    child: const Text('Register'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: ElevatedButton(
+                    style: style1,
+                    onPressed: () {},
+                    child: const Text('View'),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Registeration",
+                  style: GoogleFonts.lato(
+                      textStyle: const TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                  )),
+                )
+              ],
+            ),
+            Flexible(
+              child: Form(
+                key: _formKey,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: TextFormField(
+                          autofocus: false,
+                          decoration: const InputDecoration(
+                            labelText: 'Name ',
+                            labelStyle: TextStyle(fontSize: 20.0),
+                            border: OutlineInputBorder(),
+                            errorStyle: TextStyle(
+                                color: Colors.redAccent, fontSize: 15),
+                          ),
+                          controller: nameController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please Enter Name';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: TextFormField(
+                          autofocus: false,
+                          decoration: const InputDecoration(
+                            labelText: 'Email ',
+                            labelStyle: TextStyle(fontSize: 20.0),
+                            border: OutlineInputBorder(),
+                            errorStyle: TextStyle(
+                                color: Colors.redAccent, fontSize: 15),
+                          ),
+                          controller: emailController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please Enter Email';
+                            } else if (!value.contains('@')) {
+                              return 'Please Enter Valid Email';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: TextFormField(
+                          autofocus: false,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            labelText: 'Password ',
+                            labelStyle: TextStyle(fontSize: 20.0),
+                            border: OutlineInputBorder(),
+                            errorStyle: TextStyle(
+                                color: Colors.redAccent, fontSize: 15),
+                          ),
+                          controller: passwordController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please Enter Password';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: TextFormField(
+                          autofocus: false,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            labelText: 'Password ',
+                            labelStyle: TextStyle(fontSize: 20.0),
+                            border: OutlineInputBorder(),
+                            errorStyle: TextStyle(
+                                color: Colors.redAccent, fontSize: 15),
+                          ),
+                          controller: passwordController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please Enter Password';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 12, vertical: 4),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(3),
+                              border: Border.all(
+                                  color: const Color.fromARGB(
+                                      255, 122, 121, 121))),
+                          margin: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: DropdownButton<String>(
+                            value: dropdownValue,
+                            isExpanded: true,
+                            icon: const Icon(Icons.arrow_drop_down_circle),
+                            style: const TextStyle(
+                                color: Color.fromARGB(255, 3, 3, 3),
+                                fontSize: 20),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                dropdownValue = newValue!;
+                              });
+                            },
+                            items: <String>['10A', '10B', '11A', '11B']
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                          )),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () => {clearText()},
+                            child: const Text(
+                              'Reset',
+                              style: TextStyle(fontSize: 18.0),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.blueGrey),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Validate returns true if the form is valid, otherwise false.
+                              if (_formKey.currentState!.validate()) {
+                                setState(() {
+                                  name = nameController.text;
+                                  email = emailController.text;
+                                  password = passwordController.text;
+                                  addUser();
+                                  clearText();
+                                });
+                              }
+                            },
+                            child: const Text(
+                              'Register',
+                              style: TextStyle(fontSize: 18.0),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
